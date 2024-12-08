0635b2ca1a8bff6b9893ae22e5a18f1b7003eedd
FelipeAlvarado02 <falvcha2003@g.educaand.es>
2024-12-08 21:00:46 +0100

    mi salud mental y el netlify no pueden ir de la mano

--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -1,34 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LayoutPublic from '../layout/PublicLayout';
+import PaimonConfuse from '../assets/img/Lumine_404.png';
+import '../sass/pages/_Error.scss';
 
 const Error404 = () => {
   return (
     <LayoutPublic>
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        textAlign: 'center',
-        color: '#333',
-      }}>
-        <h1 style={{ fontSize: '80px', margin: '0' }}>404</h1>
-        <h2 style={{ fontSize: '24px', margin: '10px 0' }}>Paimon says that this page is not found</h2>
-        <p style={{ fontSize: '16px', marginBottom: '20px' }}>
-          Traveler! Did we get lost again?
-        </p>
-        <Link to="/" style={{
-          textDecoration: 'none',
-          padding: '10px 20px',
-          backgroundColor: '#007BFF',
-          color: '#fff',
-          borderRadius: '5px',
-          fontSize: '16px',
-        }}>
-          Go Back Home
-        </Link>
+      <div className="error-404">
+        <h1>404</h1>
+        <h2>Paimon says that this page is not found</h2>
+        <img src={PaimonConfuse} alt="Paimon Confused" />
+        <p>Traveler! Did we get lost again?</p>
+        <Link to="/" className="back-home">Go Back Home</Link>
       </div>
     </LayoutPublic>
   );
