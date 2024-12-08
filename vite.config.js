62d2c254cafeb936656300cae65079bf3fb47821
FelipeAlvarado02 <falvcha2003@g.educaand.es>
2024-12-08 21:25:09 +0100

    creo que va

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Genshin_Api/', // Reemplaza <repo-name> con el nombre de tu repositorio en GitHub
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Configura '@' como alias para la carpeta src
