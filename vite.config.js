27a55bde10e721ee900f8abd254e9c1ed0b82693
FelipeAlvarado02 <falvcha2003@g.educaand.es>
2024-12-08 21:12:38 +0100

    por favor, funciona

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/<Genshin_Api>/', // Reemplaza <repo-name> con el nombre de tu repositorio en GitHub
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),  // Configura '@' como alias para la carpeta src
+      '@': path.resolve(__dirname, 'src'), // Configura '@' como alias para la carpeta src
     },
   },
-})
+});
